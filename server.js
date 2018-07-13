61329e2dcc42df7fc9609109ce8752570788e66b
Ivan Milicevic <ivmilicevic1@gmail.com>
2018-07-13 20:51:39 +0200

    Set express to serve react build folder

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 // Import routes
 const jobs = require('./routes/api/jobs');
@@ -7,6 +8,18 @@ const app = express();
 
 
 app.use('/api/jobs', jobs);
+
+// Serve static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
+
+app.use(express.static('client/build'));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
 
 // Uses port 80 on deployed server or port 5000 locally
 const port = process.env.port || 5000;
